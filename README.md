26c520e06d4eba7bb24a49d9a56a782d7d6af1a3
Rashad Landrum <rland7596@gmail.com>
2023-07-10 19:21:22 -0400

    Updates

--- README.md
+++
@@ -1,6 +0,0 @@
-# MatchMade Pics
-This is a website about finding matching pictures for friends or someone special for your profile or lockscreen.
-
-## Get Started
-1) clone the repo
-2) run npm start
--- src/components/NotFound.js
+++
@@ -1,9 +0,0 @@
-function NotFound() {
-    return (
-        <div>
-            <h2>Oops! You seem to be lost.</h2>
-            <p>Tap one of the categories to find your match!</p>
-        </div>
-    )
-}
-export default NotFound;
