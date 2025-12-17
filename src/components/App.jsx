56b1880c42b5961691a0abdd05957342f1f3d66f
Rland12 <rland7596@gmail.com>
2025-12-17 16:32:56 -0500

    add kofi link

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -30,16 +30,16 @@ const SOCIAL_LINKS = [
       </svg>
     ),
   },
-  // {
-  //   key: "kofi",
-  //   label: "Ko-fi",
-  //   href: "https://ko-fi.com/matchmade",
-  //   icon: (
-  //     <svg viewBox="0 0 24 24" aria-hidden="true" focusable="false">
-  //       <path d="M5 6h11.5a3.5 3.5 0 0 1 0 7H15a4 4 0 0 1-4 4H7a2 2 0 0 1-2-2V6zm2 2v8h2a2 2 0 0 0 2-2V8H7zm9.5 0H15v3h1.5a1.5 1.5 0 0 0 0-3z" />
-  //     </svg>
-  //   ),
-  // },
+  {
+    key: "kofi",
+    label: "Buy me a hot cocoa",
+    href: "https://ko-fi.com/matchmade",
+    icon: (
+      <svg viewBox="0 0 24 24" aria-hidden="true" focusable="false">
+        <path d="M5 6h11.5a3.5 3.5 0 0 1 0 7H15a4 4 0 0 1-4 4H7a2 2 0 0 1-2-2V6zm2 2v8h2a2 2 0 0 0 2-2V8H7zm9.5 0H15v3h1.5a1.5 1.5 0 0 0 0-3z" />
+      </svg>
+    ),
+  },
 ];
 
 function App() {
